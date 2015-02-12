6d9ec6b48be883a348c4b3e37c8af7b6bbcfabeb
Rob Mac <maccelerated@gmail.com>
2015-02-11 19:26:33 -0800

    testing

--- src/main/java/client/CLI.java
+++ src/main/java/client/CLI.java
@@ -4,12 +4,10 @@
 
 package client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.util.Arrays;
 
 public class CLI {
 
@@ -19,14 +17,21 @@ public class CLI {
         byte[] buffer = new byte[64];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
+        boolean found = false;
+
         while (true) {
 
             socket.receive(packet);
 
-            String msg = new String(buffer, 0, packet.getLength());
             System.out.print(packet.getAddress().getHostName());
             System.out.print(": ");
-            System.out.println(msg);
+            System.out.println(Arrays.toString(buffer));
+
+
+            DatagramPacket something = new DatagramPacket(buffer, packet.getLength());
+            something.setAddress(packet.getAddress());
+            something.setPort(packet.getPort());
+            socket.send(something);
 
             packet.setLength(buffer.length);
 
